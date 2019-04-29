10496c708a1078bcc75bdad6fc3c581274dd7c38
abrandao <andersonbrandaolustosa@gmail.com>
2019-04-29 11:57:46 -0300

    validations

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,7 @@
 class Order < ApplicationRecord
   belongs_to :restaurant
+
+  validates :name, presence: true
+  validates :phone_number, presence: true
+  validates :total_value, presence: true
 end
--- app/models/product_category.rb
+++ app/models/product_category.rb
@@ -2,4 +2,7 @@ class ProductCategory < ApplicationRecord
   belongs_to :restaurant
 
   has_many :order_products
+
+  validates :restaurant
+  validates :title, presence: true
 end
